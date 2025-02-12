558d7666849373cc0d3317fb104d7b9c8ce22b4a
AliaHassanAkl <101278855+AliaHassanAkl@users.noreply.github.com>
2025-02-12 12:11:36 +0200

    update project section and skills section
    Updates include:
    - buttons styling and hovers 
    - skills section with new style 
    - project section title style update 
    - add some animation to the intro section

--- js/main.js
+++ js/main.js
@@ -12,3 +12,36 @@ scrollingElements.forEach(Element => {
     })
    
 })
+
+
+// Select all skill bars
+const skillBars = document.querySelectorAll('.skillLevel');
+
+// Observer setup
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.animationPlayState = 'running';
+      }
+    });
+  },
+  { threshold: 0.5 } // Trigger when 50% of the element is visible
+);
+
+// Observe each skill bar
+skillBars.forEach((bar) => observer.observe(bar));
+
+
+const text ="Welcome To My World!";
+let index= 0;
+const speed =40;
+
+function typeWriter(){
+  if (index < text.length){
+    document.getElementById('wlc').textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter,speed);
+  }
+}
+typeWriter();
